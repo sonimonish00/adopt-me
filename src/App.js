b23a8e687aa0003117ed796c7a2ebdfe0ca6546e
Monish Soni <sonimonish00@users.noreply.github.com>
2022-05-10 21:31:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 const e = React.createElement;
-const App = () => { 
-    // createElement(element, props/attribs of elements, child of elements)
-    return e(
-    "div",
-    {},
-    e("h1", {}, "Adopt Me!")
-    );
+const Pet = (props) => {
+  return e("div", {}, [
+    e("h1", {}, props.name),
+    e("h2", {}, props.animal),
+    e("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  // createElement(element, props/attribs of elements, child of elements)
+  return e("div", {}, [
+    e("h1", {}, "Adopt Me!"),
+    e(Pet,{
+      name : "Luna",
+      animal : "Dog",
+      breed : "Havanese"
+    }),
+    e(Pet,{
+      name : "Pepper",
+      animal : "Bird",
+      breed : "Cockatiel"
+    }),
+    e(Pet,{
+      name : "Doink",
+      animal : "Cat",
+      breed : "Mix"
+    }),
+  ]);
 };
 // Component Hierarchy : Root => (App : div => h1 )
-const container = document.querySelector('#root');
+const container = document.querySelector("#root");
 const root = ReactDOM.createRoot(container);
 root.render(e(App));
