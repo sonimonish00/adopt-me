b76e7e13bf8a89ce8a4df377f3e38b2ab8b921b6
Monish Soni <sonimonish00@users.noreply.github.com>
2022-05-15 18:18:33 +0530

    Little Updates

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,9 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
-  constructor() {
-    super();
-    this.state = { loading: true };
-  }
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
@@ -20,10 +18,11 @@ class Details extends Component {
       return <h2>loading … </h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
-
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
     return (
       <div className="details">
+        <Carousel images={images} />;
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
